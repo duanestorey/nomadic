d01f39138e7fa2b7442e487a46bac0b6caa35b51
Unknown <duanestorey@gmail.com>
2018-11-01 17:09:39 +0100

    Added Green Icon

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -51,9 +51,17 @@
 		        [90, 180]
 		    ]
 		});
+		var greenIcon = new L.Icon({
+		  iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+		  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+		  iconSize: [25, 41],
+		  iconAnchor: [12, 41],
+		  popupAnchor: [1, -34],
+		  shadowSize: [41, 41]
+		});
 
 		@if($location)
-			L.marker([{{ $location->lat }}, {{ $location->lon }}], {})
+			L.marker([{{ $location->lat }}, {{ $location->lon }}], {icon: greenIcon})
 			 .addTo(map)
 			 .bindPopup('{{ $location->user->name }} is currently in {{ $location->city }} {{ $location->country }}');
 		@endif
