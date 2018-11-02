49b73ce385a25096ce641e16a3a31843518f24e1
Unknown <duanestorey@gmail.com>
2018-11-02 20:00:03 +0100

    Icons

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,11 +18,12 @@
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
 
     <!-- home screen icon precomposed - 128x128 -->
-    <link rel="apple-touch-icon-precomposed" href="/images/icons/icon-128.png"/> 
+    <link rel="apple-touch-icon-precomposed" href="/images/icons/Icon-128.png"/> 
 
     <!-- home screen icon defaults - 48x48 -->
-    <link rel="apple-touch-icon" href="/images/icons/icon-48.png"/> 
-    <link rel="icon" href="/images/icons/icon-48.png" /> 
+    <link rel="apple-touch-icon" href="/images/icons/Icon-48.png"/> 
+    <link rel="icon" href="/images/icons/Icon-48.png" /> 
+    <link rel="icon" sizes="196x196" href="/images/icons/Icon-196.png"> 
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
